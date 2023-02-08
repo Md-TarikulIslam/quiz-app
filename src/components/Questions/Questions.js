94687c6b9f46dd9573949c01c53b2df2e43b80a8
MdTarikulIslam <nayon1820@gmail.com>
2023-02-08 15:26:25 +0600

    display info

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Questions = () => {
+    const [questions, setQuestions] = useState([])
     useEffect(()=>{
         fetch('question.json')
         .then(res=>res.json())
-        .then(data=>console.log(data))
-    })
+        .then(data=>setQuestions(data))
+    },[])
     return (
         <div>
+          <h2>{questions.length}</h2>
           {
-
+            questions.map(question=>console.log(question))
           }
         </div>
     );
