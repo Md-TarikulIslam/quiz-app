6cdb4bf727182742df0ec46b430d6feb1c3421ed
MdTarikulIslam <nayon1820@gmail.com>
2023-02-13 17:23:59 +0600

    load quiz one by one

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,29 +1,173 @@
 import React, { useEffect, useState } from 'react';
-import Question from '../Question/Question';
+import './Questions.css'
+
+
+const allQuestion = [
+  {
+    "id": "1",
+    "question": "What file extension (file type) will you use to save a website file? ",
+    "options": [
+     "html",
+     ".web",
+     ".html",
+     "chrome"
+    ],
+    "answer": ".html"
+  },
+  {
+    "id": "2",
+    "question": "What is the meaning of HTML?",
+    "options": [
+     "HyperText Makeup Language",
+     "How To Meet Ladies",
+     "HyperText Markup Language",
+     "Himel Tomar Mama Lage"
+    ],
+    "answer": "HyperText Markup Language"
+  },
+  {
+    "id": "3",
+    "question": "How would you write a paragraph for a website (pay attention)?",
+    "options": [
+     "<p/>paragraph</p>",
+     "<p>I am a paragraph<p>",
+     "<p I am a paragraph>",
+     "<p>I am a Paragraph</p>"
+    ],
+    "answer": "<p>I am a Paragraph</p>"
+  },
+  {
+    "id": "4",
+    "question": "How many HTML heading tags are there?",
+    "options": [
+     "8",
+     "10",
+     "6",
+     "3"
+    ],
+    "answer": "6"
+  },
+  {
+    "id": "5",
+    "question": "Which one is the third biggest heading tag?",
+    "options": [
+     "<h3>I am the one</h3>",
+     "<h1>I am the one</h1>",
+     "<h2>I am the one</h2>",
+     "<h4>I am the one</h4>"
+    ],
+    "answer": "<h3>I am the one</h3>"
+  },
+  {
+    "id": "6",
+    "question": "If you click on an anchor tag, where will it usually open (if needed, try it out yourself).",
+    "options": [
+     "It will open in a new tab",
+     "It will open in the current tab",
+     "Abbu basai, can not open the door now",
+     "It will open new browser window"
+    ],
+    "answer": "It will open in the current tab"
+  },
+  {
+    "id": "7",
+    "question": "How will you write a list item tag containing the word mobile?",
+    "options": [
+     "<ul>mobile</ul>",
+     "<ol>mobile</ol>",
+     "<li>mobile</li>",
+     "<item>mobile</item>"
+    ],
+    "answer": "<li>mobile</li>"
+  },
+  {
+    "id": "8",
+    "question": "How would you write a button tag?",
+    "options": [
+     "<button></button>",
+     "<buton></buton>",
+     "<btn></btn>",
+     "<buttn></buttn>"
+    ],
+    "answer": "<button></button>"
+  },
+  {
+    "id": "9",
+    "question": "Which attribute is not a part of the input tag?",
+    "options": [
+     "type",
+     "src",
+     "placeholder",
+     "name"
+    ],
+    "answer": "src"
+  },
+  {
+    "id": "10",
+    "question": "Which tag will you use to add style on your website?",
+    "options": [
+     "<css></css>",
+     "<color></color>",
+     "<style></style>",
+     "<head></head>"
+    ],
+    "answer": "<style></style>"
+  }
+]
 
 const Questions = () => {
-    const [questions, setQuestions] = useState([])
-    useEffect(()=>{
-        fetch('question.json')
-        .then(res=>res.json())
-        .then(data=>setQuestions(data))
-    },[])
+    const [count, setCount] = useState(0)
+    const [timer, setTimer] = useState(60000)
+    const [score, setScore] = useState(0)
+    const [showScore, setShowScore] = useState(false)
+   
 
+
+
+  const handleButton = (answer)=>{
+    if(answer)
+    {
+      setScore(score+1);
+
+    }
+    const nextQuestion = count+1;
+    if(nextQuestion<allQuestion.length)
+    {
+      setCount(nextQuestion)
+    }
+    else{
+      setShowScore(true);
+    }
+  }
    
     return (
         <div>
-          {/* <h2>{questions.length}</h2> */}
-          {
-            // setInterval(function(){
-              questions.map((question)=> <Question
-              key={question.id}
-              question={question}
-              ></Question>)
+        {showScore? (
+          <div>
+            Your score is {score} out of {allQuestion.length}
+          </div>
+        ) : (
+            <div className='items'>
+              <div className='question-item'>
+              {allQuestion[count].id}
+              <span>. </span>
+              {allQuestion[count].question}
+              </div>
+            <div className='option-area'>
+            {
+              allQuestion[count].options.map((option)=>(
+                
+                <button className='btn' onClick={()=>handleButton(option.answer)}>{option}</button>
+              ))
             }
-            {/* , 1000)}  */}
+            </div>
+          
+            </div>
+        )}
+          
       
         </div>
     );
 };
 
-export default Questions;
+export default Questions;  
